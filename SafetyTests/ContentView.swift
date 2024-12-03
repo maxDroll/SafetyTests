c6840df6519c859f0bdb01e8e84a13eb4bb4a482
Max Droll <mdroll6088@stu.d214.org>
2024-12-03 11:17:25 -0600

    Initial Commit

---
+++ SafetyTests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafetyTests
+//
+//  Created by Maxwell K. Droll on 12/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafetyTests/SafetyTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyTestsApp.swift
+//  SafetyTests
+//
+//  Created by Maxwell K. Droll on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SafetyTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
